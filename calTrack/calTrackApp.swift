9c614abcc9d51bae5fcbbf817bc3258ac9110708
GABRIELROSENDO <gabrielrosendo72@gmail.com>
2024-11-09 19:35:56 -0700

    Initial Commit

---
+++ calTrack/calTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  calTrackApp.swift
+//  calTrack
+//
+//  Created by Gabriel on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct calTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
